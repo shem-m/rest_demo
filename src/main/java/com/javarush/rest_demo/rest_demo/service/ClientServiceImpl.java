96649d6257826c6db1001a193ac0a87dae8854bd
ShemMarta <ShemMarta@users.noreply.github.com>
2020-12-06 20:38:00 +0300

    Client,ClientService and ClientServiceImpl added

---
+++ src/main/java/com/javarush/rest_demo/rest_demo/service/ClientServiceImpl.java
@@ -0,0 +1,37 @@
+package com.javarush.rest_demo.rest_demo.service;
+
+import com.javarush.rest_demo.rest_demo.model.Client;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClientServiceImpl implements ClientService {
+
+    private static final Map<Integer, Client> CLIENT_REPOSITORY_MAP = new HashMap();
+
+    @Override
+    public void create(Client client) {
+
+    }
+
+    @Override
+    public List<Client> readAll() {
+        return null;
+    }
+
+    @Override
+    public Client read(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Client client, int id) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+}
